763e9ab91b6a22c37fdb924cf9b5a7a0412972fa
David Sánchez (Laptop) <davidsanrubio@gmail.com>
2021-12-31 13:29:27 +0100

    ✨ Added test workflow and deploy

---
+++ deploy.js
@@ -0,0 +1,25 @@
+const SftpUpload = require("sftp-upload"),
+  fs = require("fs");
+
+const options = {
+    host: process.env.DEPLOY_HOST,
+    username: process.env.DEPLOY_USER,
+    password: process.env.DEPLOY_PASS,
+    path: "./dist/portfolio",
+    remoteDir: "./",
+    dryRun: false,
+  },
+  sftp = new SftpUpload(options);
+
+sftp
+  .on("error", function (err) {
+    throw err;
+  })
+  .on("uploading", function (progress) {
+    console.log("Uploading", progress.file);
+    console.log(progress.percent + "% completed");
+  })
+  .on("completed", function () {
+    console.log("Upload Completed");
+  })
+  .upload();
