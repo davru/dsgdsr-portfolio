ff1f7c1a4568f6e1ea0b550a734ffbf73c9732db
David Sánchez (Laptop) <davidsanrubio@gmail.com>
2022-01-04 14:24:51 +0100

    ✨ Scroll/back-to-top functionality added

--- src/app/components/back-to-top/back-to-top.component.ts
+++ src/app/components/back-to-top/back-to-top.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-back-to-top',
@@ -6,7 +6,27 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./back-to-top.component.scss']
 })
 export class BackToTopComponent implements OnInit {
+    public isAtTop = true;
+    private topOffset = 30;
+
     constructor() {}
 
     ngOnInit(): void {}
+
+    @HostListener('window:scroll', ['$event'])
+    _onScroll(event: any): void {
+        this.isAtTop = event.srcElement?.scrollingElement?.scrollTop < this.topOffset;
+    }
+
+    public scroll(): void {
+        this.isAtTop ? this.scrollTo(this.vhToPixels(100)) : this.scrollTo(0);
+    }
+
+    private scrollTo(px: number): void {
+        window.scrollTo(document.body.scrollWidth, px);
+    }
+
+    private vhToPixels(vh: number): number {
+        return Math.round(window.innerHeight / (100 / vh));
+    }
 }
