24da7fab621863e45d91e4625b28ef0064331509
David Sánchez (Laptop) <davidsanrubio@gmail.com>
2022-01-04 23:13:45 +0100

    📈 Added fathom analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,18 @@
-import { Component, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ThemeModes, ThemeService } from './services/theme.service';
+import { load } from 'fathom-client';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  constructor(
-    private elementRef: ElementRef,
-    public themeService: ThemeService
-  ) {
-    themeService.setTheme(localStorage.getItem('theme-mode') as ThemeModes ?? ThemeModes.Light);
+export class AppComponent implements OnInit {
+  constructor(public themeService: ThemeService) {
+    themeService.setTheme(localStorage.getItem('theme-mode') as ThemeModes ?? ThemeModes.Dark);
+  }
+
+  ngOnInit(): void {
+    load('BRJUSAOF');
   }
 }
