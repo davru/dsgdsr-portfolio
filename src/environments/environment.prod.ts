97bd5028954d680dd34e9798736fe7ab72ac0313
David Sánchez (Laptop) <davidsanrubio@gmail.com>
2022-04-23 16:49:12 +0200

    🕵🏽‍♂️ updated supabase secrets

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  supabaseUrl: 'https://drafonojgzjqtgxguqfs.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRyYWZvbm9qZ3pqcXRneGd1cWZzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY1MDcyNTEzOSwiZXhwIjoxOTY2MzAxMTM5fQ.4WcEVGugSxW9Ao1FWq9wfMkYUkmf82NeaqX30SNDCJ4'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   supabaseUrl: 'https://drafonojgzjqtgxguqfs.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRyYWZvbm9qZ3pqcXRneGd1cWZzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY1MDUzNTY2MiwiZXhwIjoxOTY2MTExNjYyfQ.2amZxMAWMFq0nkf0DQwn3_dWsYsNJ8elBpqxWzQ19Xw'
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRyYWZvbm9qZ3pqcXRneGd1cWZzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY1MDcyNTEzOSwiZXhwIjoxOTY2MzAxMTM5fQ.4WcEVGugSxW9Ao1FWq9wfMkYUkmf82NeaqX30SNDCJ4'
 };
