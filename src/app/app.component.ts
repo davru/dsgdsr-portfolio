c545ab0f146d70fdf72bb6435a9958facbc21052
David Sánchez (Laptop) <davidsanrubio@gmail.com>
2022-05-08 22:47:46 +0200

    📈 Modified analytics manager

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ThemeModes, ThemeService } from './services/theme.service';
-import { load } from 'fathom-client';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   constructor(public themeService: ThemeService) {
     themeService.setTheme(localStorage.getItem('theme-mode') as ThemeModes ?? ThemeModes.Dark);
-  }
-
-  ngOnInit(): void {
-    // ENV VARIABLE
-    load('BRJUSAOF', {
-      spa: 'history'
-    });
   }
 }
