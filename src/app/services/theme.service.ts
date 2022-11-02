e3492df5d07b63815229efd4c01eef62c785b04a
David Sánchez (Laptop) <davidsanrubio@gmail.com>
2022-11-02 22:00:58 +0100

    🗑  Remove unused code

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -12,33 +12,6 @@ export enum ThemeModes {
 export class ThemeService {
     public theme: ThemeModes = ThemeModes.Dark;
 
-    /*private styleVariables = {
-    '--bg-color': {
-      light: 'white',
-      dark: '#23252c',
-    },
-    '--scrollbar-bg': {
-      light: '#333333',
-      dark: '#ffffff',
-    },
-    '--bg-secondary-color': {
-      light: '#30333e',
-      dark: '#323744',
-    },
-    '--text-color': {
-      light: '#161618',
-      dark: 'white',
-    },
-    '--color-text-main': {
-      light: '#fb24a2',
-      dark: '#ff69c1',
-    },
-    '--terminal-shadow': {
-      light: '0px 10px 40px #52525290',
-      dark: '0px 10px 20px #00000030',
-    },
-  };*/
-
     constructor(@Inject(DOCUMENT) private document: Document) {}
 
     public setTheme(theme: ThemeModes, renderer: Renderer2): void {
